9cae98605587e3d5963867e63225b95e16b2ff07
Muhamad Ulin Nuha <muhamadulinnuha653@gmail.com>
2026-03-03 23:51:57 +0700

    create model product

---
+++ lib/features/cart/domain/entities/product.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Product {
+  final String id;
+  final String name;
+  final String price;
+
+  const Product ({
+    required this.id,
+    required this.name,
+    required this.price,
+  });
+}
